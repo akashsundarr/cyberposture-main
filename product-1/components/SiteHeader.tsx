a858f581416348fb4a289ab046b18151b4c0ca24
Akash Sundar <akashsundarrr@gmail.com>
2026-09-11 23:40:19 +0530

    hello

--- product-1/components/SiteHeader.tsx
+++ product-1/components/SiteHeader.tsx
@@ -87,7 +87,7 @@ export function SiteHeader() {
           </Link>
 
           <span className="text-sm font-medium text-on-surface-variant/60">
-            .ai
+            
           </span>
         </div>
 
